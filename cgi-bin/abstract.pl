8d2dea8398df7584616e8cedfb84ea842c640092
Qblack <blac2410@mylaurier.ca>
2015-01-30 15:33:51 -0500

    Finished q5 1

--- cgi-bin/abstract.pl
+++ cgi-bin/abstract.pl
@@ -9,34 +9,33 @@ use CGI qw(:standard -debug);
 
 my ($name) = (param("name"));
 
-sub trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
-
-my @paragraphs = ();
+ my @paragraphs;
 open(IN, "<$name") or die "Error - Could not open $name";
+{   local $/ = "\r\n\r\n";
+    @paragraphs = <IN>;
+}
 
-my $paragraphStart=1;
-my @sentence;
-while(my $line = <IN>){
-    if($line =~ /^\s*$/){
-        $paragraphStart = 1;
-    }
-    if($paragraphStart){
-        my @piece = split(/\./, $line);
-        push(@sentence, @piece[0]);
-        if(@piece>1){
-            $paragraphStart = 0;
-            print "@sentence.\n\n";
-            @sentence = "";
-        }
-    }
+my @sentences;
+
+my $paragraph_count = 0;
+foreach my $paragraph (@paragraphs) {
+  if ($paragraph !~ /\./) {
+    print $paragraph;
+  } else {
+    @sentences = split(/\./, $paragraph);
+    print $sentences[0].".\n\n";
+  }
+  $paragraph_count++;
 }
 
 close(IN);
 
+
+
 open(IN, "<$name") or die "Error - Could not open $name";
     my %frequency;
     my %excluded_words = ("a"=>0,"and"=>1, "it"=>2,"this"=>3, "that"=>4, "the"=>5,"of"=>6,"."=>7);
-
+    my $line_count = 0;
     while (my $line = <IN>) {
         my @temp = split (/[,.;=>()'"\s]+/, $line);
         foreach my $word (@temp) {
@@ -49,15 +48,20 @@ open(IN, "<$name") or die "Error - Could not open $name";
                 }
             }
         }
+        if($line !~ /^\s*$/){
+            $line_count++;
+        }
     }
     my $count=0;
-    my @sorted = sort { $frequency{$b} <=> $frequency{$a} or $b cmp $a } keys %frequency;
-    print("Top Five Most Frequent Words:\n");
-    while($count<5){
-        my $word = $sorted[$count];
-        printf("%-15s %s\n", $word, $frequency{$word});
-        $count++;
-    }
+    my @sorted_frequencies = sort { $frequency{$b} <=> $frequency{$a} or $b cmp $a } keys %frequency;
 
 close(IN);
 
+printf("There are %s lines.\n",$line_count);
+printf("There are %s paragraphs.\n", $paragraph_count);
+print("Top Five Most Frequent Words:\n");
+while($count<5){
+    my $word = $sorted_frequencies[$count];
+    printf("%-15s %s\n", $word, $frequency{$word});
+    $count++;
+}
